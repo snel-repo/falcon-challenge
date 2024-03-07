9cd28d2eb6f4bba1e044f8521dcff6b33db4ac2b
Joel Ye <joelye9@gmail.com>
2024-03-07 14:15:19 -0500

    parity

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='falcon_challenge',
-    version='0.0.2',
+    version='0.1.0',
 
     url='https://github.com/snel-repo/stability-benchmark',
     author='Joel Ye',
