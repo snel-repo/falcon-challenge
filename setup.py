cb0e3ad8a7e78a2d40e28d6d886225822a049fff
Joel Ye <joelye9@gmail.com>
2024-03-07 20:26:39 -0500

    inc version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='falcon_challenge',
-    version='0.1.0',
+    version='0.1.1',
 
     url='https://github.com/snel-repo/stability-benchmark',
     author='Joel Ye',
