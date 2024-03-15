3438ae02ec1db92c905bca2c2a44dc069c75cd4c
bkarpowicz3 <brianna.karpowicz@gmail.com>
2024-03-15 15:56:23 -0400

    m1 in progress 2

--- preproc/m1_reachgrasp_preprocv2.py
+++ preproc/m1_reachgrasp_preprocv2.py
@@ -3,6 +3,7 @@
 from pynwb import NWBFile
 from pynwb import NWBHDF5IO
 import numpy as np
+import pandas as pd 
 import scipy.signal as signal
 
 from scipy.io import loadmat
@@ -46,7 +47,7 @@ rouse_base_dir = "/snel/share/share/data/rouse/RTG/"
 MONKEY = "L"
 EXP_DATE = "20120924"
 
-if len(sys.argv) > 1:
+if len(sys.argv) > 2:
     EXP_DATE = sys.argv[1]
     MONKEY = sys.argv[2]
 
@@ -112,10 +113,38 @@ fs_cont = float(f_emg['EMGSettings']['samp_rate'][0][0][0][0])
 t_offset = f_emg['EMGSettings']['analog_start_time'][0][0][0][0]
 
 #%% 
-n_units = np.unique(f_spk['SpikeSettings']['channels'][()]).shape[0]
+channels = np.squeeze(f_spk['SpikeSettings']['channels'][()])
+all_spike_times = []
+for i in range(1, 97):
+    ch_spk_times = []
+    where_chan = np.where(channels == i)[0]
+    for j in where_chan:
+        spike_times = f_spk[f_spk['AllSpikeTimes'][j][0]]
+        if spike_times.shape[0] == 1:
+            spike_times = spike_times[0]
+        else:
+            spike_times = np.squeeze(spike_times)
+        ch_spk_times.append(spike_times)
+    if len(ch_spk_times) > 1:
+        ch_spk_times = np.concatenate(ch_spk_times)
+    elif len(ch_spk_times) == 1:
+        ch_spk_times = ch_spk_times[0]
+    all_spike_times.append(ch_spk_times)
+
+#%% 
+# n_units = np.unique(f_spk['SpikeSettings']['channels'][()]).shape[0]
 array_group_by_chan = f_spk['SpikeSettings']['array_by_channel'][0]
-array_group_by_chan = [ chr(array_id) for array_id in array_group_by_chan.tolist()]
-elec_id_by_chan = f_spk['SpikeSettings']['unique_channel_num'][0]
+array_group_by_chan = [chr(array_id) for array_id in array_group_by_chan.tolist()] # this is for each channels that has spikes 
+array_group_by_elec = []
+for i in range(1, 97): 
+    if i in channels: 
+        array_group_by_elec.append(
+            array_group_by_chan[np.where(channels == i)[0][0]]
+        )
+    else:
+        array_group_by_elec.append(None)
+
+# elec_id_by_chan = f_spk['SpikeSettings']['unique_channel_num'][0]
 
 #%% 
 
@@ -135,8 +164,9 @@ def convert_to_NWB(
     emg_data, 
     emg_names, 
     t_offset, 
-    n_units,
-    elec_id_by_chan,
+    # n_units,
+    # elec_id_by_chan,
+    spike_times,
     array_group_by_chan,
     spike_time_thresh=[None, None],
     split_label='in_day_oracle' #calibration, eval
@@ -179,12 +209,12 @@ def convert_to_NWB(
     for ix in range(exp_trial_ids.size):
         # ix = trial_order[i]
         number = exp_trial_ids[ix]
-        start_time = trial_start_times[ix]
-        end_time = trial_end_times[ix]
-        gocue_time = gocue_times[ix]
-        move_onset_time = move_onset_times[ix]
-        contact_time = contact_times[ix]
-        reward_time = reward_times[ix]
+        start_time = pd.to_datetime(trial_start_times[ix], unit='s').round('20ms').value * 1e-9 
+        end_time = pd.to_datetime(trial_end_times[ix], unit='s').round('20ms').value * 1e-9
+        gocue_time = pd.to_datetime(gocue_times[ix], unit='s').round('20ms').value * 1e-9
+        move_onset_time = pd.to_datetime(move_onset_times[ix], unit='s').round('20ms').value * 1e-9
+        contact_time = pd.to_datetime(contact_times[ix], unit='s').round('20ms').value * 1e-9
+        reward_time = pd.to_datetime(reward_times[ix], unit='s').round('20ms').value * 1e-9
         tgt_loc = locations[ix]
         tgt_obj = obj_names[ix]
         obj_id = object_ids[ix]
@@ -206,20 +236,22 @@ def convert_to_NWB(
 
     logger.info("Adding acquisition data")
 
+    target_fs = 50
     t_cont = (np.arange(0, emg_data.shape[0])/fs_cont).round(4)
 
-    emg_mts = create_multichannel_timeseries(
-        "emg_raw", emg_names, emg_data, timestamps=t_cont, unit="mV"
-    )
-    nwbfile.add_acquisition(emg_mts)
-    # create processing module
-    emg_filt = nwbfile.create_processing_module(
-        "emg_filtering_module",
-        "module to perform emg pre-processing from raw to rectified emg",
-    )
+    # emg_mts = create_multichannel_timeseries(
+    #     "emg_raw", emg_names, emg_data, timestamps=t_cont, unit="mV"
+    # )
+    # nwbfile.add_acquisition(emg_mts)
+    # # create processing module
+    # emg_filt = nwbfile.create_processing_module(
+    #     "emg_filtering_module",
+    #     "module to perform emg pre-processing from raw to rectified emg",
+    # )
 
     # extract acquisition data
-    raw_emg = nwbfile.acquisition["emg_raw"]
+    # raw_emg = nwbfile.acquisition["emg_raw"]
+    raw_emg = emg_data 
 
     notch_cent_freq = [60, 180, 200, 300, 400]
     notch_bw_freq = [2, 2, 2, 2, 2]
@@ -228,55 +260,70 @@ def convert_to_NWB(
     hp_cutoff_freq = 65  # Hz
 
     # 1) notch filter
-    notch_emg = apply_filt_to_multi_timeseries(
-        raw_emg,
-        apply_notch_filt,
-        "emg_notch",
-        fs_cont,
-        notch_cent_freq,
-        notch_bw_freq,
-    )
+    # notch_emg = apply_filt_to_multi_timeseries(
+    #     raw_emg,
+    #     apply_notch_filt,
+    #     "emg_notch",
+    #     fs_cont,
+    #     notch_cent_freq,
+    #     notch_bw_freq,
+    # )
+    notch_emg = apply_notch_filt(raw_emg, fs_cont, notch_cent_freq, notch_bw_freq)
+
 
     # 2) high pass filter
-    hp_emg = apply_filt_to_multi_timeseries(
-        notch_emg, apply_butter_filt, "emg_hp", fs_cont, "high", hp_cutoff_freq
-    )
+    # hp_emg = apply_filt_to_multi_timeseries(
+    #     notch_emg, apply_butter_filt, "emg_hp", fs_cont, "high", hp_cutoff_freq
+    # )
+    hp_emg = apply_butter_filt(notch_emg, fs_cont, "high", hp_cutoff_freq)
 
     # 3) rectify
-    rect_emg = apply_filt_to_multi_timeseries(hp_emg, rectify, "emg")
+    # rect_emg = apply_filt_to_multi_timeseries(hp_emg, rectify, "emg")
+    rect_emg = rectify(hp_emg)
 
     # clipping and quantile normalization of EMG
     CLIP_Q = 0.99
     SCALE_Q = 0.95
-    clip_emg = apply_filt_to_multi_timeseries(
-        rect_emg, apply_clipping, 'emg_clip', CLIP_Q
-    )
-    scale_emg = apply_filt_to_multi_timeseries(
-        clip_emg, apply_scaling, 'emg_scale', SCALE_Q
-    )
+    clip_emg = apply_clipping(rect_emg, CLIP_Q)
+    # clip_emg = apply_filt_to_multi_timeseries(
+    #     rect_emg, apply_clipping, 'emg_clip', CLIP_Q
+    # )
+    # scale_emg = apply_filt_to_multi_timeseries(
+    #     clip_emg, apply_scaling, 'emg_scale', SCALE_Q
+    # )
+    scale_emg = apply_scaling(clip_emg, SCALE_Q)
 
     # resample all the processed EMG data to 20ms bins (50Hz) 
-    resamp_emg = apply_filt_to_multi_timeseries(
-        scale_emg, resample_column, 'emg_resample', 50, fs_cont
-    )
+    # resamp_emg = apply_filt_to_multi_timeseries(
+    #     scale_emg, resample_column, 'emg_resample', target_fs, fs_cont
+    # )
+    resamp_emg = resample_column(scale_emg, target_fs, fs_cont)
 
     # rectify again 
-    rerect_emg = apply_filt_to_multi_timeseries(resamp_emg, rectify, 'emg_rerect')
+    # rerect_emg = apply_filt_to_multi_timeseries(resamp_emg, rectify, 'emg_rerect')
+    rerect_emg = rectify(resamp_emg)
 
     # apply low pass filter 
-    preprocessed_emg = apply_filt_to_multi_timeseries(
-        rerect_emg, apply_butter_filt, 'preprocessed_emg', 50, 'low', 10
-    )
+    # preprocessed_emg = apply_filt_to_multi_timeseries(
+    #     rerect_emg, apply_butter_filt, 'preprocessed_emg', 50, 'low', 10
+    # )
+    preprocessed_emg = apply_butter_filt(rerect_emg, target_fs, 'low', 10)
 
     # add each step to processing module
-    emg_filt.add_container(notch_emg)
-    emg_filt.add_container(hp_emg)
-    emg_filt.add_container(rect_emg)
-    emg_filt.add_container(clip_emg)
-    emg_filt.add_container(scale_emg)
-    emg_filt.add_container(resamp_emg)
-    emg_filt.add_container(rerect_emg)
-    emg_filt.add_container(preprocessed_emg)
+    # emg_filt.add_container(notch_emg)
+    # emg_filt.add_container(hp_emg)
+    # emg_filt.add_container(rect_emg)
+    # emg_filt.add_container(clip_emg)
+    # emg_filt.add_container(scale_emg)
+    # emg_filt.add_container(resamp_emg)
+    # emg_filt.add_container(rerect_emg)
+    start_time = (spike_time_thresh[0] if spike_time_thresh[0] else 0) * fs_cont / target_fs
+    t_new = (np.arange(int(start_time), int(start_time) + preprocessed_emg.shape[0])/target_fs).round(4)
+    emg_proc_mts = create_multichannel_timeseries(
+        "preprocessed_emg", emg_names, preprocessed_emg, timestamps=t_new, unit="mV"
+    )
+    nwbfile.add_acquisition(emg_proc_mts)
+    # emg_filt.add_container(preprocessed_emg)
 
     logger.info("Adding spiking data")
 
@@ -348,54 +395,69 @@ def convert_to_NWB(
     nwbfile.units = Units(
         name="units", description="Sampled at 30 kHz with anti-alias", resolution=1/fs_cont
     )
-    orig_elec_ids = np.unique(elec_id_by_chan)
-    new_elec_ids = np.arange(orig_elec_ids.size)
-    elec_id_map = { o_ix: n_ix for (o_ix, n_ix) in zip(orig_elec_ids, new_elec_ids)}
-    array_elec_groups = [*map(elec_group_map.get, array_group_by_chan)]
+    # orig_elec_ids = np.unique(elec_id_by_chan)
+    # new_elec_ids = np.arange(orig_elec_ids.size)
+    # elec_id_map = { o_ix: n_ix for (o_ix, n_ix) in zip(orig_elec_ids, new_elec_ids)}
+    # array_elec_groups = [*map(elec_group_map.get, array_group_by_chan)]
+    array_elec_groups = [] 
+    for arr_id in array_group_by_chan:
+        if arr_id is not None:
+            array_elec_groups.append(elec_group_map[arr_id])
+        else:
+            array_elec_groups.append(None)
 
-    for i in range(n_units):
-        elec_id = orig_elec_ids[i]
+    for elec_id in range(1, 97):
+        # elec_id = orig_elec_ids[i]
         try:
             nwbfile.add_electrode(
-                id=int(elec_id_map[elec_id]),
+                id=elec_id, #int(elec_id_map[elec_id]),
                 x=np.nan,
                 y=np.nan,
                 z=np.nan,
                 imp=np.nan,
                 location="M1",
-                group=array_elec_groups[i],
+                group=array_elec_groups[elec_id-1],
                 filtering="unknown",
             )
         except ValueError:
             logger.info('Electrode already added to table')
 
-        where_elec = np.where(elec_id_by_chan == elec_id)[0]
-        all_spike_times = []
-        for j in where_elec:
-            spike_times = f_spk[f_spk['AllSpikeTimes'][j][0]]
-            if spike_times.shape[0] == 1:
-                spike_times = spike_times[0]
-            else:
-                spike_times = np.squeeze(spike_times)
-            spike_times = (spike_times - t_offset).round(4)
-            all_spike_times.append(spike_times)
+        # where_elec = np.where(elec_id_by_chan == elec_id)[0]
+        # all_spike_times = []
+        # for j in where_elec:
+        #     spike_times = f_spk[f_spk['AllSpikeTimes'][j][0]]
+        #     if spike_times.shape[0] == 1:
+        #         spike_times = spike_times[0]
+        #     else:
+        #         spike_times = np.squeeze(spike_times)
+        #     spike_times = (spike_times - t_offset).round(4)
+        #     all_spike_times.append(spike_times)
 
-        all_spike_times = np.concatenate(all_spike_times)
+        # all_spike_times = np.concatenate(all_spike_times)
+        all_spike_times = np.array(spike_times[elec_id - 1])
+        if len(all_spike_times) >= 1: 
+            all_spike_times = (all_spike_times - t_offset).round(4)
 
         start_cutoff, stop_cutoff = spike_time_thresh
         if start_cutoff is None: 
-            start_cutoff = t_cont[0]
+            start_cutoff = t_new[0]
         if stop_cutoff is None: 
-            stop_cutoff = t_cont[-1]
-        
+            stop_cutoff = t_new[-1]
         # ensure spike times are within bound of continuous data
         keep_mask = (all_spike_times > start_cutoff) & (all_spike_times < stop_cutoff)
+        valid_spike_times = all_spike_times[keep_mask] if len(all_spike_times) >= 1 else all_spike_times
+        # if valid_spike_times is not None:
         nwbfile.add_unit(
-            id=i,
-            spike_times=all_spike_times[keep_mask],
-            electrodes=[elec_id_map[elec_id]],
-            obs_intervals=[[start_cutoff, stop_cutoff + 1/fs_cont]],
+            id=elec_id,
+            spike_times=np.squeeze(valid_spike_times),
+            electrodes=[elec_id],
+            obs_intervals=[[start_cutoff, stop_cutoff]],
         )
+        # else: 
+        #     nwbfile.add_unit(
+        #         id=i,
+        #         electrodes=[elec_id],
+        #     )
     
     nwb_path = path.join(SAVE_PATH, split_label)
     if not path.exists(nwb_path):
@@ -448,9 +510,8 @@ if IS_TEST_DS:
         emg_data[:calib_end_ind_emg, :], 
         emg_names, 
         t_offset, 
-        n_units,
-        elec_id_by_chan,
-        array_group_by_chan,
+        all_spike_times,
+        array_group_by_elec,
         spike_time_thresh=[0, trial_end_times[:calibration_num][-1]], 
         split_label='calibration' #'in_day_oracle' #calibration, eval
     )
@@ -474,10 +535,9 @@ if IS_TEST_DS:
         emg_data[eval_start_ind_emg:, :],
         emg_names,
         t_offset,
-        n_units,
-        elec_id_by_chan,
-        array_group_by_chan,
-        spike_time_thresh=[trial_start_times[-eval_num:][0], emg_data.shape[0]/fs_cont],
+        all_spike_times,
+        array_group_by_elec,
+        spike_time_thresh=[trial_start_times[-eval_num:][0], None],
         split_label='eval'
     )
 
@@ -499,42 +559,41 @@ if IS_TEST_DS:
         emg_data[:eval_start_ind_emg, :],
         emg_names,
         t_offset,
-        n_units,
-        elec_id_by_chan,
-        array_group_by_chan,
+        all_spike_times,
+        array_group_by_elec,
         spike_time_thresh=[0, trial_start_times[-eval_num:][0]],
         split_label='in_day_oracle'
     )
 
 else: 
     logger.info("Creating full training dataset")
-    # need full dataset 
+    # first 60%
+    eval_start_ind_emg = int(trial_start_times[-eval_num:][0] * fs_cont)
     convert_to_NWB(
         fs_cont,
-        trial_start_times,
-        trial_end_times,
-        gocue_times,
-        move_onset_times,
-        contact_times,
-        reward_times,
-        generic_cond_ids,
-        object_ids,
-        object_names,
-        locations,
-        exp_trial_ids,
-        emg_data,
+        trial_start_times[:-eval_num],
+        trial_end_times[:-eval_num],
+        gocue_times[:-eval_num],
+        move_onset_times[:-eval_num],
+        contact_times[:-eval_num],
+        reward_times[:-eval_num],
+        generic_cond_ids[:-eval_num],
+        object_ids[:-eval_num],
+        object_names[:-eval_num],
+        locations[:-eval_num],
+        exp_trial_ids[:-eval_num],
+        emg_data[:eval_start_ind_emg, :],
         emg_names,
         t_offset,
-        n_units,
-        elec_id_by_chan,
-        array_group_by_chan,
-        spike_time_thresh=[None, None],
-        split_label='full'
+        all_spike_times,
+        array_group_by_elec,
+        spike_time_thresh=[None, trial_start_times[-eval_num:][0]],
+        split_label='train'
     )
 
     logger.info("Creating minival split")
     # and minival dataset which is last EVAL_RATIO of train data 
-    eval_start_ind_emg = int(trial_start_times[-eval_num:][0] * fs_cont)
+    # eval_start_ind_emg = int(trial_start_times[-eval_num:][0] * fs_cont)
     convert_to_NWB(
         fs_cont,
         trial_start_times[-eval_num:],
@@ -551,10 +610,9 @@ else:
         emg_data[eval_start_ind_emg:, :],
         emg_names,
         t_offset,
-        n_units,
-        elec_id_by_chan,
-        array_group_by_chan,
-        spike_time_thresh=[trial_start_times[-eval_num:][0], emg_data.shape[0]/fs_cont],
+        all_spike_times,
+        array_group_by_elec,
+        spike_time_thresh=[trial_start_times[-eval_num:][0], None],
         split_label='minival'
     )
 
