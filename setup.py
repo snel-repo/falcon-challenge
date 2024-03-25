4d1af0f3a649d5986a5ffd5bcac8803f464a8f38
claytonwashington <washington.534@osu.edu>
2024-03-25 15:58:54 -0400

    setup.py: add torch, remove decoder_demos exclusion

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Joel Ye',
     author_email='joelye9@gmail.com',
 
-    packages=find_packages(exclude=['data_demos', 'decoder_demos', 'data']),
+    packages=find_packages(exclude=['data_demos', 'data']),
     install_requires=[
         'numpy',
         'hydra-core',
@@ -19,6 +19,7 @@ setup(
         'seaborn',
         'scikit-learn',
         'pynwb',
+        'torch',
     ],
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
