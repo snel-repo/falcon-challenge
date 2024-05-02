585152da617dced7f6da096aec70c9713c2d87c0
Joel Ye <joelye9@gmail.com>
2024-05-02 19:44:22 -0400

    tick m1 neuron count

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='falcon_challenge',
-    version='0.3.2',
+    version='0.3.3',
 
     url='https://github.com/snel-repo/stability-benchmark',
     author='Joel Ye',
