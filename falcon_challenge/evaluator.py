8d3e415b9f723ca1a4fa2d90f6eeca8d9064d66e
claytonwashington <washington.534@osu.edu>
2024-05-10 15:33:27 -0400

    make evlauation changes work with m2

--- falcon_challenge/evaluator.py
+++ falcon_challenge/evaluator.py
@@ -94,8 +94,8 @@ DATASET_HELDINOUT_MAP = {
         ]
     },
     'm2': {
-        'held_in': ['2020-10-19-Run1', '2020-10-19-Run2', '2020-10-20-Run1', '2020-10-20-Run2', '2020-10-27-Run1', '2020-10-27-Run2', '2020-10-28-Run1'],
-        'held_out': ['2020-10-30-Run1', '2020-10-30-Run2', '2020-11-18-Run1', '2020-11-19-Run1', '2020-11-24-Run1', '2020-11-24-Run2'],
+        'held_in': ['Run1_20201019', 'Run2_20201019', 'Run1_20201020', 'Run2_20201020', 'Run1_20201027', 'Run2_20201027', 'Run1_20201028'],
+        'held_out': ['Run1_20201030', 'Run2_20201030', 'Run1_20201118', 'Run1_20201119', 'Run1_20201124', 'Run2_20201124'],
     },
 }
 
@@ -197,14 +197,14 @@ def evaluate(
             dataset_pred = dataset_pred[:dataset_mask.shape[0]] # In case excess timesteps are predicted due to batching, reduce
             if dataset in DATASET_HELDINOUT_MAP[datasplit]['held_in']:
                 if 'h2' not in datasplit:
-                    session_id = next(s for s in HELD_IN_KEYS[getattr(FalconTask, datasplit)] if s in dataset)
+                    session_id = next(s.replace('L_','') for s in HELD_IN_KEYS[getattr(FalconTask, datasplit)] if s.replace('L_','') in dataset)
                     dset_len_dict['held_in'][session_id].append(dataset_mask.shape[0])
                 pred_dict['held_in'].append(dataset_pred)
                 tgt_dict['held_in'].append(dataset_tgt)
                 mask_dict['held_in'].append(dataset_mask)
             elif dataset in DATASET_HELDINOUT_MAP[datasplit]['held_out']:
                 if not 'h2' in datasplit:
-                    session_id = next(s for s in HELD_OUT_KEYS[getattr(FalconTask, datasplit)] if s in dataset)
+                    session_id = next(s.replace('L_','') for s in HELD_OUT_KEYS[getattr(FalconTask, datasplit)] if s.replace('L_','') in dataset)
                     dset_len_dict['held_out'][session_id].append(dataset_mask.shape[0])
                 pred_dict['held_out'].append(dataset_pred)
                 tgt_dict['held_out'].append(dataset_tgt)
