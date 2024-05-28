154706654eeacb66c49427d993e2aa7af9427bbc
claytonwashington <washington.534@osu.edu>
2024-05-28 16:54:22 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='falcon_challenge',
-    version='0.3.8',
+    version='0.3.9',
 
     url='https://github.com/snel-repo/stability-benchmark',
     author='Joel Ye',
